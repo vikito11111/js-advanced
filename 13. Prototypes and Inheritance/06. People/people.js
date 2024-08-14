4e03c19578af3785f0744ca050d3d84c4c61846d
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-08-14 11:19:25 +0300

    Add files via upload

---
+++ 13. Prototypes and Inheritance/06. People/people.js
@@ -0,0 +1,73 @@
+function solve() {
+    class Employee {
+        constructor(name, age) {
+            if (new.target === Employee) {
+                throw new Error('Cannot instantiate directly.');
+            }
+
+            this.name = name,
+            this.age = age,
+            this.salary = 0,
+            this.tasks = [];
+        }
+    
+        work() {
+            let currentTask = this.tasks.shift();
+    
+            console.log(this.name + currentTask);
+    
+            this.tasks.push(currentTask);
+        }
+    
+        collectSalary() {
+            console.log(`${this.name} received ${this.salary} this month.`);
+        }
+    }
+    
+    class Junior extends Employee {
+        constructor(name, age) {
+            super(name, age);
+            this.salary = 0;
+            this.tasks = [
+                ' is working on a simple task.'
+            ];
+        }
+    }
+    
+    class Senior extends Employee {
+        constructor(name, age) {
+            super(name, age);
+            this.tasks = [
+                ' is working on a complicated task.',
+                ' is taking time off work.',
+                ' is supervising junior workers.'
+            ];
+        }
+    }
+    
+    class Manager extends Employee {
+        constructor(name, age) {
+            super(name, age);
+            this.devidend = 0;
+            this.tasks = [
+                ' scheduled a meeting.',
+                ' is preparing a quarterly report.'
+            ];
+        }
+
+        getSalary() {
+            return this.salary + this.devidend;
+        }
+
+        collectSalary() {
+            console.log(`${this.name} received ${this.getSalary()} this month.`);
+        }
+    }
+
+    return { 
+        Employee, 
+        Junior, 
+        Senior, 
+        Manager 
+    };
+}
