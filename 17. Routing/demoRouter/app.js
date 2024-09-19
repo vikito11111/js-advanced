92bbd7f71a9a2fd70ded25de5ae53cf4da9784a7
vikito11111 <142398365+vikito11111@users.noreply.github.com>
2024-09-19 12:24:28 +0300

    Add files via upload

---
+++ 17. Routing/demoRouter/app.js
@@ -0,0 +1,10 @@
+import Router from "./router.js";
+
+var router = new Router({
+    home: {
+        logState: function(state) {
+            console.log(state);
+        }
+    }
+});
+router.initialize();
---
+++ 17. Routing/demoRouter/router.js
@@ -0,0 +1,57 @@
+export default class Router {
+    constructor(routes) {
+        this.routes = routes;
+        this.currentPath = '';
+        this.currentQueryString = '';
+        this.state = {};
+    };
+
+    initialize() {
+        history.pushState({}, '', '/');
+
+        setInterval(this.listen.bind(this), 50);
+
+        document.addEventListener('click', (e) => {
+            if (e.target.href && e.target.origin === document.location.origin) {
+                e.preventDefault();
+                e.stopPropagation();
+                history.pushState({}, '', e.target.href);
+            }
+        });
+    };
+
+    listen() {
+        if (this.currentPath !== decodeURIComponent(document.location.pathname) || this.currentQueryString !== decodeURIComponent(document.location.search)) {
+            this.currentPath = decodeURIComponent(document.location.pathname) || '';
+            this.currentQueryString = decodeURIComponent(document.location.search) || '';
+
+            if (this.currentQueryString !== '') {
+                this.state = this.currentQueryString
+                .match(/[A-ZА-Я0-9]+=[A-ZА-Я0-9]+/gi)
+                .map((el) => el.split('='))
+                .reduce((acc, el) => {
+                    if (Array.isArray(el) && el.length === 2) {
+                        acc[el[0]] = el[1];
+                    }
+
+                    return acc;
+                }, {});
+            }
+            else {
+                this.state = history.state;
+            }
+
+            this.loadPage();
+        }
+    };
+
+    loadPage() {
+        var route = this.currentPath
+        .split('/')
+        .filter((el) => !!el);
+
+        if (route.length >= 2 && this.routes.hasOwnProperty(route[0]) && typeof(this.routes[route[0]][route[1]]) === 'function') {
+            this.routes[route[0]][route[1]](this.state);
+        }
+    }
+}
